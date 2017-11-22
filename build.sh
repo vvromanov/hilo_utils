70a414c29b80461bb4fb854deb4b2c6f6fafb65e
Vladimir Romanov <vromanov@gmail.com>
2017-11-22 11:06:55 +0300

    Fix systemd library

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -fr build
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=On -DTRAVIS=On
-make VERBOSE=1
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=On
+# -DTRAVIS=On
+make VERBOSE=1 -j8
 
