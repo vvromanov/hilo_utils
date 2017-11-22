6dccba7be61a15f069dc83e3f124deaa9354d415
Vladimir Romanov <vromanov@gmail.com>
2017-11-22 11:10:58 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 rm -fr build
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=On
-# -DTRAVIS=On
-make VERBOSE=1 -j8
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=On -DTRAVIS=On
+make -j8
 
